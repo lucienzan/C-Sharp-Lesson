e6c06aa43b3c49ef7be6b3c62291652817415499
Danielxcott <changwilliam826@gmail.com>
2023-06-25 14:59:43 +0630

    Added enum in advance topic

---
+++ Advance Topic/Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+  internal class Program
+  {
+    public enum Year
+    {
+      January,
+      February = 2, 
+      March,
+      April,
+      May = 7,
+      June,
+      July
+    }
+    static void Main(string[] args)
+    {
+
+      System.Console.WriteLine((int)Year.January); // 0
+      System.Console.WriteLine(Year.March); //March
+      System.Console.WriteLine((int)Year.February); //2
+      System.Console.WriteLine((int)Year.May); //7
+      System.Console.WriteLine("<----->");
+      foreach (var item in Enum.GetValues(typeof(Year))){
+        System.Console.WriteLine(item.ToString());
+      }
+    }
+  }
+}
